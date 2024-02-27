8e10fb0fb0bf8d8a74a222dddf2b49892c57791e
Sr-Wild <wilderytg@gmail.com>
2024-02-27 19:37:59 -0400

    actulizacion de codigo y favicon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const $button = document.querySelector('button')
 
 const option  = {
-    mimeType:'video/webm;codecs=vp9,opus',
+    mimeType:"video/webm;codecs=vp9,opus",
     audioBitsPerSecond:2500000,
     videoBitsPerSecond:2500000
 }
@@ -14,10 +14,10 @@ const recorded_Chunks = [];
 $button.addEventListener('click', async () => {
   const media = await navigator.mediaDevices.getDisplayMedia(setting)
   const media_recorder = new MediaRecorder(media,option)
-  media_recorder.ondataavailable = handleDataAvailable;
+  media_recorder.ondataavailable = handle_data_available;
   media_recorder.start()
 
-  function handleDataAvailable(event) {
+  function handle_data_available(event) {
     if (event.data.size > 0) {
       recorded_Chunks.push(event.data);
       download();
@@ -25,9 +25,7 @@ $button.addEventListener('click', async () => {
     }
   }
   function download() {
-    const blob = new Blob(recorded_Chunks, {
-      type: "video/webm",
-    });
+    const blob = new Blob(recorded_Chunks);
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     document.body.appendChild(a);
@@ -37,13 +35,6 @@ $button.addEventListener('click', async () => {
     a.click();
     window.URL.revokeObjectURL(url);
   }
-
-  /* media_recorder.addEventListener("dataavailable", (e) => {
-    const link = document.createElement("a")
-    link.href = URL.createObjectURL(e.data)
-    link.download = "captura"
-    link.click()
-  }) */
   const [video] = media.getVideoTracks()
   video.addEventListener("ended", () => {
     media_recorder.stop()
