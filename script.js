7f7e1b6739f51ab099728fd360a4d21c6b99fcfd
Wilder Pérez <88911910+Sr-Wild@users.noreply.github.com>
2024-02-27 10:48:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $button.addEventListener('click', async () => {
   
   const type_video_format = ['video/webm;codecs="vp9.02.10.10.01.09.16.09.01,opus"']
 
-  const mediarecorder = new MediaRecorder(media, {
+  const media_recorder = new MediaRecorder(media, {
     mimeType:type_video_format,
     audioBitsPerSecond:128000,
     videoBitsPerSecond:2500000
@@ -17,10 +17,10 @@ $button.addEventListener('click', async () => {
 
   const [video] = media.getVideoTracks()
   video.addEventListener("ended", () => {
-    mediarecorder.stop()
+    media_recorder.stop()
   })
 
-  mediarecorder.addEventListener("dataavailable", (e) => {
+  media_recorder.addEventListener("dataavailable", (e) => {
     const link = document.createElement("a")
     link.href = URL.createObjectURL(e.data)
     link.download = "captura"
